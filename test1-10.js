3d19c9119f1f0c8ed870d1b5bc0da73a50e01d0d
Abnv06 <157219542+Abnv06@users.noreply.github.com>
2024-06-07 17:59:03 +0530

    100-test
    100 day 100 test

---
+++ test1-10.js
@@ -0,0 +1,104 @@
+// line number 4///// Program to check number is odd or even//////////////////////////////
+
+// let givenNumber = 10;
+
+// function EvenandOdd(givenNumber){
+// if(givenNumber%2 === 0){
+//     return `Given number ${givenNumber} is Even`;
+// } else 
+// return `Given number ${givenNumber} is Odd`;
+// }
+// console.log(EvenandOdd(givenNumber));
+
+// line number 5//////add two numbers////////////////////////////////////////////////////////////////
+
+// function addnumber(a,b){
+//     return a+b;
+// }
+
+// console.log(addnumber(5,6))
+
+// line number 7///////// program to swap two variables /////////////////////////////////////////////////////
+
+// let a = 5;
+// let b = 10;
+
+
+// let temp = a;
+// a = b;
+// b = temp;
+
+// console.log(`swap => a = ${a}, b = ${b}`);
+
+// line number 8/////////To find largent number /////////////////////////////////////////////////////
+
+// function findLargestNumber(array) {
+//     if (array.length === 0) {
+//         throw new Error("Array is empty");
+//     }
+
+//     let largest = array[0];
+//     for (let i = 1; i < array.length; i++) {
+//         if (array[i] > largest) {
+//             largest = array[i];
+//         }
+//     }
+//     return largest;
+// }
+
+// // Example usage:
+// let numbers = [5, 10, 7, 3, 8];
+// let largest = findLargestNumber(numbers);
+// console.log(`The largest number in the array is ${largest}`);
+
+///////////line 11 Km to Miles///////////////////////////////////////////////////////////////////////////////////////////
+
+// function KmToMiles(kilometers) {
+//     const milesPerKilometer = 0.621371;
+//     return kilometers * milesPerKilometer;
+// }
+
+// let kilometers = 15;
+// let miles = KmToMiles(kilometers);
+// console.log(`${kilometers} kilometers is equal to ${miles} miles.`);
+
+
+///////////line 12 squre root Km to Miles///////////////////////////////////////////////////////////////////////////////////////////
+
+// let number = 25
+
+// let result = Math.sqrt(number);
+// console.log(`The square root of ${number} is ${result}`);
+
+///////////line 14 Check if the number is positive, negative, or zero//////////////////////////////////////////////////
+
+// let number = 10;
+
+// // 
+// if (number > 0) {
+//     console.log(`${number} is positive.`);
+// } else if (number < 0) {
+//     console.log(`${number} is negative.`);
+// } else {
+//     console.log(`${number} is zero.`);
+// }
+
+///////////line 15 check prime numbner //////////////////////////////////////////////////
+
+// let number = 16;
+
+
+// function primeNumber(number){
+
+//     if (number === 1 )
+//     return "1 is not valid number"
+
+//     if (number%2 == 0){
+//       return "given number is not prime"
+//    } else (number%2 !== 0) 
+//     return "given number is prime"
+// }
+
+// console.log(primeNumber(number))
+
+///////////line 15 check prime numbner //////////////////////////////////////////////////
